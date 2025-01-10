930c9494c1394948cd367e166becdd61e1a11dc7
Jürgen Leschner <jldec@users.noreply.github.com>
2025-01-10 11:35:49 +0000

    Show country if no city

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -2,7 +2,7 @@
 // https://developers.cloudflare.com/pages/functions/api-reference/#eventcontext
 // https://developers.cloudflare.com/workers/runtime-apis/request/#incomingrequestcfproperties
 export function onRequest(context) {
-return new Response(`Hello from ${context.request.cf.city}!
+return new Response(`Hello from ${context.request.cf.city || context.request.cf.country}!
 The date is ${new Date().toDateString()}
 The time is ${new Date().toLocaleTimeString()}`);
 }
